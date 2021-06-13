d983553f827f2e660d53ee3e8db9e15812a84948
Spartan0nix <l.humblot74@gmail.com>
2021-06-13 10:25:43 +0200

    added CodeMirror

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,15 @@
       <section class="regex">
         <h4>Expression régulière</h4>
         <div class="input-text">
-          <input type="text" id="regex-input" placeholder="Entrez votre expression régulière ..." v-model="regex">
-          <pre></pre>
+          <textarea id="regex-input" placeholder="Entrez votre expression régulière ..." v-model="regex" style="display: none"></textarea>
+          <!-- <input type="text" id="regex-input" placeholder="Entrez votre expression régulière ..." v-model="regex"> -->
         </div>
       </section>
       <section class="test-string">
         <h4>Chaine de test</h4>
-        <div class="input-text">
-          <textarea 
-            name="test-string" 
-            id="test-string" 
-            cols="30" 
-            rows="10" 
-            placeholder="Entrez votre chaine de test ..."
-            v-model="testString"></textarea>
-          <pre contenteditable="true" @input="update"></pre>
+        <!-- <div class="input-text"> -->
+        <div>
+          <textarea id="test-string" placeholder="Entrez votre chaîne de test ..." v-model="testString" style="display: none"></textarea>
         </div>
       </section>
     </div>
@@ -26,7 +20,12 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, watch } from 'vue';
+import { defineComponent, onMounted, ref, watch } from 'vue';
+import * as CodeMirror from 'codemirror';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/dracula.css';
+
+import 'codemirror/addon/display/placeholder';
 
 export default defineComponent({
   name: 'App',
@@ -35,20 +34,33 @@ export default defineComponent({
     const regex = ref<string>('');
     const testString = ref<string>('');
 
-    watch([regex, testString], () => {
-      if(regex.value == "" || testString.value == ""){
-        return
-      }
 
-      [...testString.value].forEach(c => {
-        console.log(c)
+    onMounted(() => {
+      const regexEditor = CodeMirror.fromTextArea(document.getElementById('regex-input') as HTMLTextAreaElement, {
+        lineNumbers: true,
+        theme: 'dracula'
       })
 
+      const testStringEditor = CodeMirror.fromTextArea(document.getElementById('test-string') as HTMLTextAreaElement, {
+        lineNumbers: true,
+        theme: 'dracula'
+      })
+      
+      regexEditor.on("change", (element) => {
+        console.log(element.getValue());
+      })
     })
+    
+    // watch([regex, testString], () => {
+    //   if(regex.value == "" || testString.value == ""){
+    //     return
+    //   }
 
-    const update = (content: string) => {
-      console.log(content)
-    }
+    //   [...testString.value].forEach(c => {
+    //     console.log(c)
+    //   })
+
+    // })
 
     const match = (match: boolean) => {
       let testStrinContainer = document.getElementById("test-string");
@@ -59,7 +71,7 @@ export default defineComponent({
       }
     }
 
-    return { regex, testString, update }
+    return { regex, testString }
   }
 });
 </script>
@@ -83,22 +95,7 @@ h4 {
 .test-string {
   margin: 1rem 0 0 0;
 }
-/* .test-string .input-text{
-  position: relative;
-}
-.test-string #test-string {
-  background-color: transparent;
-}
-.test-string pre {
-  position: absolute;
-  top: 0;
-  left: 0;
-  widows: 100%;
-  height: 100%;
-} */
-.match {
-  background-color: lightblue;
-}
+
 .container section {
   border: 1px solid red;
 }
