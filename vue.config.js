eecea630791819008c625b3b880ae1f8bb2d861f
Spartan0nix <l.humblot74@gmail.com>
2021-06-16 21:56:22 +0200

    updated vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,7 @@
  * @type {import('@vue/cli-service').ProjectOptions}
  */
  module.exports = {
-    // options...
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/js-regex-debugger/'
+    : '/'
   }
