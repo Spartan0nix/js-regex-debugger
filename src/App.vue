9cfc3ed6c240c35394d1a55e9db15e161fee4c75
Spartan0nix <l.humblot74@gmail.com>
2021-06-16 21:39:33 +0200

    Design

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <main class="app">
+  <main>
+    <h2>Tester vos expressions régulières</h2>
     <div class="container">
       <section class="regex">
-        <h4>Expression régulière</h4>
+        <h3>Expression régulière</h3>
         <div class="input-text">
           <textarea id="regex-input" placeholder="Entrez votre expression régulière ..." v-model="regex" style="display: none"></textarea>
-          <!-- <input type="text" id="regex-input" placeholder="Entrez votre expression régulière ..." v-model="regex"> -->
         </div>
       </section>
       <section class="test-string">
-        <h4>Chaine de test</h4>
-        <!-- <div class="input-text"> -->
-        <div>
+        <h3>Chaine de test</h3>
+        <div class="input-text">
           <textarea id="test-string" placeholder="Entrez votre chaîne de test ..." v-model="testString" style="display: none"></textarea>
         </div>
       </section>
@@ -24,6 +23,7 @@ import { defineComponent, onMounted, ref, watch } from 'vue';
 import * as CodeMirror from 'codemirror';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/dracula.css';
+import 'codemirror/theme/nord.css'
 
 import 'codemirror/addon/display/placeholder';
 
@@ -34,12 +34,11 @@ export default defineComponent({
     const regex = ref<string>('');
     const testString = ref<string>('');
 
-
     onMounted(() => {
       const regexEditor = CodeMirror.fromTextArea(document.getElementById('regex-input') as HTMLTextAreaElement, {
         lineNumbers: true,
         theme: 'dracula',
-        mode: "none"
+        mode: "none",
       })
 
       const testStringEditor = CodeMirror.fromTextArea(document.getElementById('test-string') as HTMLTextAreaElement, {
@@ -60,7 +59,8 @@ export default defineComponent({
     CodeMirror.defineMode("regex",function() {
       return {
         token: function(stream, state) {
-          try {
+          if(regex.value){
+            try {
               var regexExp = new RegExp(regex.value, 'g')
               if(stream.match(regexExp)) {
                 stream.eat(regexExp)
@@ -69,22 +69,17 @@ export default defineComponent({
                 stream.next();
                 return null;
               }
-          } catch (error) {
+            } catch (error) {
+              stream.next();
+              return null;
+            }
+          } else {
             stream.next();
             return null;
           }
         } 
       } 
     })
-
-    const match = (match: boolean) => {
-      let testStrinContainer = document.getElementById("test-string");
-      if(match){
-        testStrinContainer?.classList.contains('match') ? '' : testStrinContainer?.classList.add('match')
-      } else {
-        testStrinContainer?.classList.contains('match') ? testStrinContainer?.classList.remove('match') : ''
-      }
-    }
 
     return { regex, testString }
   }
@@ -94,25 +89,44 @@ export default defineComponent({
 <style>
 @import './assets/css/index.css'; 
 
+main {
+  /* padding-top: 5rem; */
+}
+
+h2 {
+  text-align: center;
+  padding: 2rem;
+}
+
 .container {
   width: 70%;
-  margin: 3rem auto;
-  background-color: #F3F5F6;
-  color: #0B090A;
+  margin: 0 auto;
+  background-color: var(--secondary);
+  color: var(--font);
   padding: 1.5rem;
 }
 
-h4 {
-  border: 1px solid yellow;
+.container section {
+  border-top: 2px solid var(--red);
+}
+
+h3 {
   margin: 0.8rem 0.4rem 0.4rem 0;
+  font-size: 24px;
+}
+
+.regex .input-text {
+  height: 3rem;
 }
 
 .test-string {
   margin: 1rem 0 0 0;
 }
 
-.container section {
-  border: 1px solid red;
+@media screen and (max-width: 600px) {
+  .container {
+    margin: 0;
+    width: auto;
+  }
 }
-
 </style>
